7ea3395020b5fb410ccd2045bffb2575171819d0
patel007007patel-arch <patel007007patel@gmail.com>
2025-12-19 22:03:45 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -8,10 +8,17 @@ const nextConfig = {
   },
 
   webpack: (config, { isServer }) => {
-    // SVG handling
+    // SVG handling - convert SVGs to React components
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"],
+      issuer: /\.[jt]sx?$/,
+      use: [{
+        loader: "@svgr/webpack",
+        options: {
+          typescript: true,
+          ext: "tsx",
+        },
+      }],
     });
     
     // Optimize bundle size
